5cbcf176acf2f4c6779219f9e51aa9e6cd1b98a9
gokulmk-12 <ed21b026@smail.iitm.ac.in>
2024-03-28 00:03:38 +0530

    New files and Changes

--- ik_pkg/scripts/moveit_python.py
+++ ik_pkg/scripts/moveit_python.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import tf
 import sys
 import math
 import copy
@@ -19,20 +20,49 @@ scene = moveit_commander.PlanningSceneInterface()
 
 group = moveit_commander.MoveGroupCommander("manipulator")
 
-display_traj = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory, queue_size=10)
+# display_traj = rospy.Publisher('/move_group/display_planned_path',moveit_msgs.msg.DisplayTrajectory, queue_size=10)
 
 joint_goal = group.get_current_joint_values()
+br = tf.TransformBroadcaster()
 
-joint_goal[0] = math.radians(50)
-joint_goal[1] = math.radians(90)
-joint_goal[2] = math.radians(-10)
-joint_goal[3] = math.radians(10)
-joint_goal[4] = math.radians(10)
+while not rospy.is_shutdown() :
 
-group.go(joint_goal, wait=True)
-group.plan()
+    br.sendTransform([0.6, 0.0, 0.12],[0.0, 0.0, 0.0, 1.0],rospy.Time.now(), "Obj_1", "base_link")
+    rospy.sleep(2)
 
-rospy.sleep(5)
+    joint_goal[0] = math.radians(1)
+    joint_goal[1] = math.radians(44)
+    joint_goal[2] = math.radians(-59)
+    joint_goal[3] = math.radians(-48)
+    joint_goal[4] = math.radians(-3)
+
+    group.go(joint_goal, wait=True)
+    # group.execute(joint_goal)
+    rospy.sleep(2)
+
+    br.sendTransform([0.7, 0.43, 0.15],[-0.1135, 0.11883, 0.27453, 0.94743],rospy.Time.now(), "Obj_2", "base_link")
+
+    joint_goal[0] = math.radians(31)
+    joint_goal[1] = math.radians(65)
+    joint_goal[2] = math.radians(-22)
+    joint_goal[3] = math.radians(-13)
+    joint_goal[4] = math.radians(9)
+
+    group.go(joint_goal, wait=True)
+    # group.plan()
+    rospy.sleep(3)
+
+    break
+
+    # joint_goal[0] = math.radians(40)
+    # joint_goal[1] = math.radians(90)
+    # joint_goal[2] = math.radians(-10)
+    # joint_goal[3] = math.radians(10)
+    # joint_goal[4] = math.radians(10)
+
+    # group.go(joint_goal, wait=True)
+    # group.plan()
+    # rospy.sleep(5)
 
 
 
